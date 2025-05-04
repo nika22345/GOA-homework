82229ca34c20a2c4e59cdf6f21345f3e39bb20c5
nika22345 <nikolozkalandadze06@gmail.com>
2025-05-03 20:55:47 -0500

    day 03

---
+++ day 03/homework/hw.py
@@ -0,0 +1,16 @@
+name = "Nika"
+lastname = "Kalandadze"
+age = "15"
+
+print("Hello, my name is nika and my surname is Kalandadze, and im 15 years old")
+
+a = 15
+b = 432
+c = 25
+d = 3
+e = 45
+
+print(a+b) = 447
+print(b - c) = 407
+print( d * e) = 135
+print(b / d) = 144
