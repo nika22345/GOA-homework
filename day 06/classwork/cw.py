4ba9f53a26365fb6f4a1bbd4e539b14715568443
nika22345 <nikolozkalandadze06@gmail.com>
2025-05-17 20:27:40 -0500

    Update cw.py

---
+++ day 06/classwork/cw.py
@@ -0,0 +1,21 @@
+while True:
+    print("hello")
+    print("bye")
+    print("another bye")
+
+    name = input("Enter your name: ")
+
+    i = 0
+while i < 10:
+    print(name)
+    i += 1
+
+    # while aris instruqcia
+    #loop aris gameoreba
+
+
+while True:
+    number = int(input("Enter a number: "))
+    if number > 100:
+        break
+    print("Number should be higher than 100.")
