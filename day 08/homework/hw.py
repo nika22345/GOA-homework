009d4901d8cf50df963afacc570ded21e098cc0f
nika22345 <nikolozkalandadze06@gmail.com>
2025-05-22 09:37:58 -0500

    commit

---
+++ day 08/homework/hw.py
@@ -0,0 +1,18 @@
+#დავალება: გამოიყენე for ციკლი, რომ დაბეჭდო რიცხვები 1-დან 10-მდე.
+
+
+for i in range(1 , 11, ):
+    print(i)
+
+#დაბეჭდე სიტყვა "Hello world" ხუთჯერ, ყოველი დაბეჭდვა ახალ ხაზზე.
+
+for i in range(5):
+    print("Hello world")
+
+#გამოიყენე for ციკლი, რომ დაბეჭდო მხოლოდ ლუწი რიცხვები  10.
+
+for i in range(2, 11, 2):
+    print(i)
+
+#for loop gamoiyeneba ragaceebis gameoreba
+
