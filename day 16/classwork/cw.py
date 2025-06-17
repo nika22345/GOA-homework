4b489ded7721ce035437c96865d3f7df369235a1
nika22345 <nikolozkalandadze06@gmail.com>
2025-06-17 18:03:04 -0400

    commit

---
+++ day 16/classwork/cw.py
@@ -0,0 +1,3 @@
+#htmls viyenebt web developmentshi romelic aris saitis sheqmna da ase shemdeg da komentarebs viyenebt <!-- komentari -->
+#identacia gamouiyeneba imashi rom kodi upro kargad da gasagebad chandes
+#nestingi nishnavs roca elementshi sxva elements vdebt
