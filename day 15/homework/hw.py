784d87f0bf2a536abfb5f8461f48aac3ffad8083
nika22345 <nikolozkalandadze06@gmail.com>
2025-06-19 20:47:30 -0400

    commit

---
+++ day 15/homework/hw.py
@@ -0,0 +1,23 @@
+#1)
+#კომენტარებით ახსენით რისთვის გამოიყენება Len()-ფუნქცია
+
+#len gamoiyeneba saitebshi an striqonebshi elementebis raodenobis dasatvlelad
+
+#2)
+#კომენტარებით ახსენით რისთვის გამოიყენება List-ი
+
+#listi aris ragaca roca shegidzlia rom sxvadasxva elementebi ertdroulad sheinaxo
+
+#3)
+#კომენტარებით ახსენით რა განსხვავებაა List-სა და ცვლადებ-ს შორის
+
+#cvladi inaxavs ert konketul mnishvnelobas 
+#4)
+#რისი შენახვა შეუძლია list-ს?
+
+#inaxavs ricxvebs, striqonebs booleanebs da atwilad ricxvebs
+
+#5)
+#შექმენი ლისტი, შეინახეთ სამი წიგნის სახელი. დაბეჭდე მთელი სია და დაბეჭდე, რამდენი წიგნია მასში.
+
+
