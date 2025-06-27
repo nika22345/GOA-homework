2aecd2907c014cfaac00e1db386e7895fb0af78a
nika22345 <nikolozkalandadze06@gmail.com>
2025-06-27 09:16:17 -0400

    commit

---
+++ day 17/classwork/cw.py
@@ -0,0 +1,12 @@
+#indeqsingi gvexmareba rom movdzebnot konkretuli adgili rame siashi an sityvashi
+#indeqsit shegvidzlia rom vnaxot konkretuli elementi, shevcvalot elementi siashi da davamushavot elementi cal calke
+
+
+cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+cars[1], cars[2] = cars[2], cars[1]
+print(cars)
+
+
+txt = "hello world, python html"
+print(txt[0])
+print(txt[-1])
