edb9d26f21b7e5412ab01c56224db9070f808d7b
nika22345 <nikolozkalandadze06@gmail.com>
2025-07-03 19:09:28 -0500

    commit

---
+++ day 18/classwork/cw.py
@@ -0,0 +1,12 @@
+
+
+#ასევე ახსენი text formatting -tag ებიდან თითოეული რას შვება 
+#text formatting -tagები : <b> <i> <u> <em> <strong> 
+#და აქედან რომელია სემატიკური თაგი
+
+#sematikuri tagi iqneba em da strong
+#text formatting tagi shvebian 
+
+#<b>- teqsts muqad achvenebs
+#<i>- teqsts daxrilad achvenebs
+#<u>- xazgasmuli teqsti
