49cf01e5aceec91660bd25272689c1126820dc62
nika22345 <nikolozkalandadze06@gmail.com>
2025-07-25 09:07:14 -0500

    commit

---
+++ day 25/homework/hw.py
@@ -0,0 +1,16 @@
+ #კომენტარის სახით ჩამოთვალე ჩაშენებული ფუნქციები და რას შვება თითეული ახსენი 2) კომენატრებით ახსენით როგორ იქმენბა def ფუნქცია 3) შექმენით def ფუნქცია სახელად greet - მას უნდა ჰქონდეს 1 არგუმენტი name და უნდა დაწერე მისალმების ფუნქცი
+
+print("Hello, world!")
+name = input("What is your name? ")
+length = len(name)
+data_type = type(length)
+age = int(input("Enter your age: "))      
+price = float("19.99")                     
+number = str(100)
+
+for i in range(1, 6):                     
+    print(i)
+    numbers = [1, 2, 3]
+total = sum(numbers)    
+highest = max(numbers)                     
+lowest = min(numbers)   
